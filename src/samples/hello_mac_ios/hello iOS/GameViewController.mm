14f57ac5a8df29f20dfe2b4a03b6da93446c716d
Timo Wiren <timo.wiren@gmail.com>
2023-06-02 21:28:28 +0300

    [metal] iOS progress

--- src/samples/hello_mac_ios/hello iOS/GameViewController.mm
+++ src/samples/hello_mac_ios/hello iOS/GameViewController.mm
@@ -1,4 +1,5 @@
 #import "GameViewController.h"
+#include "renderer.h"
 
 void InitApp( unsigned width, unsigned height );
 void DrawApp();
@@ -9,6 +10,7 @@ extern id<MTLCommandBuffer> gCommandBuffer;
 @implementation GameViewController
 {
     MTKView *_view;
+    dispatch_semaphore_t inflightSemaphore;
 }
 
 - (void)viewDidLoad
@@ -22,7 +24,8 @@ extern id<MTLCommandBuffer> gCommandBuffer;
     _view.colorPixelFormat = MTLPixelFormatBGRA8Unorm_sRGB;
     _view.depthStencilPixelFormat = MTLPixelFormatDepth32Float;
     _view.sampleCount = 1;
-
+    _view.delegate = self;
+    
     if(!_view.device)
     {
         NSLog(@"Metal is not supported on this device");
@@ -35,7 +38,34 @@ extern id<MTLCommandBuffer> gCommandBuffer;
         
     InitApp( width, height );
 
-    //_view.delegate = _renderer;
+    inflightSemaphore = dispatch_semaphore_create( 2 );
+}
+
+- (void)drawInMTKView:(nonnull MTKView *)view
+{
+    @autoreleasepool
+    {
+        dispatch_semaphore_wait( inflightSemaphore, DISPATCH_TIME_FOREVER );
+
+        renderPassDescriptor = _view.currentRenderPassDescriptor;
+
+        SetDrawable( view.currentDrawable );
+
+        DrawApp();
+            
+        __block dispatch_semaphore_t block_sema = inflightSemaphore;
+        [gCommandBuffer addCompletedHandler:^(id<MTLCommandBuffer> buffer) { dispatch_semaphore_signal( block_sema ); }];
+
+        teEndFrame();
+    }
+}
+
+- (void)mtkView:(nonnull MTKView *)view drawableSizeWillChange:(CGSize)size
+{
+}
+
+- (void)encodeWithCoder:(nonnull NSCoder *)aCoder
+{
 }
 
 @end
