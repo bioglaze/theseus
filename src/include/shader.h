3079ada4809446eaa23e1703c4b99420fda0d42c
Timo Wiren <timo.wiren@gmail.com>
2023-11-17 22:51:50 +0200

    [metal] Compute Shader

--- src/include/shader.h
+++ src/include/shader.h
@@ -5,8 +5,15 @@ struct teShader
     unsigned index = 0;
 };
 
+struct ShaderParams
+{
+    unsigned readTexture;
+    unsigned writeTexture;
+};
+
 /*
     When using Vulkan, vertexFile and FragmentFile must point to a SPIR-V shader. vertexName is the name of the vertex shader main function.
 */
 teShader teCreateShader( const struct teFile& vertexFile, const teFile& fragmentFile, const char* vertexName, const char* fragmentName );
 teShader teCreateComputeShader( const teFile& file, const char* name, unsigned threadsPerThreadgroupX, unsigned threadsPerThreadgroupY );
+void teShaderDispatch( const teShader& shader, unsigned groupsX, unsigned groupsY, unsigned groupsZ, const ShaderParams& params, const char* debugName );
--- src/video/metal/shader_metal.mm
+++ src/video/metal/shader_metal.mm
@@ -3,11 +3,20 @@
 #include "te_stdlib.h"
 
 extern id <MTLLibrary> defaultLibrary;
+extern id <MTLCommandQueue> gCommandQueue;
+extern id<MTLDevice> gDevice;
+
+id<MTLTexture> TextureGetMetalTexture( unsigned index );
 
 struct ShaderImpl
 {
     id <MTLFunction> vertexProgram;
     id <MTLFunction> pixelProgram;
+    id <MTLFunction> computeProgram;
+    id <MTLComputePipelineState> computePipeline;
+    unsigned threadsPerThreadgroupX;
+    unsigned threadsPerThreadgroupY;
+    MTLSize threadgroupCounts;
 };
 
 static constexpr unsigned MaxShaders = 40;
@@ -53,3 +62,57 @@ teShader teCreateShader( const struct teFile& vertexFile, const teFile& pixelFil
     return outShader;
 }
 
+teShader teCreateComputeShader( const teFile& file, const char* name, unsigned threadsPerThreadgroupX, unsigned threadsPerThreadgroupY )
+{
+    teAssert( shaderCount < MaxShaders );
+
+    teShader outShader;
+    outShader.index = ++shaderCount;
+    
+    NSString* computeShaderName = [NSString stringWithUTF8String:name ];
+    shaders[ outShader.index ].computeProgram = [defaultLibrary newFunctionWithName:computeShaderName];
+    shaders[ outShader.index ].threadgroupCounts = MTLSizeMake( threadsPerThreadgroupX, threadsPerThreadgroupY, 1 );
+    
+    if (shaders[ outShader.index ].computeProgram == nullptr)
+    {
+        NSLog( @"Shader: Could not load %s!\n", name );
+        outShader.index = 0;
+    }
+    else
+    {
+        NSError *error = nil;
+        shaders[ outShader.index ].computePipeline = [gDevice newComputePipelineStateWithFunction:shaders[ outShader.index ].computeProgram error:&error];
+        
+        if (!shaders[ outShader.index ].computePipeline)
+        {
+            NSLog( @"Error occurred when building compute pipeline for function %s: %@", name, [error localizedDescription] );
+        }
+    }
+    
+    return outShader;
+}
+
+void teShaderDispatch( const teShader& shader, unsigned groupsX, unsigned groupsY, unsigned groupsZ, const ShaderParams& params, const char* debugName )
+{
+    teAssert( shader.index != 0 );
+    teAssert( shaders[ shader.index ].computeProgram != nil );
+
+    MTLSize threadgroups = MTLSizeMake( groupsX, groupsY, groupsZ );
+
+    id<MTLCommandBuffer> commandBuffer = [gCommandQueue commandBuffer];
+    commandBuffer.label = [NSString stringWithUTF8String:debugName ];
+    
+    id<MTLComputeCommandEncoder> commandEncoder = [commandBuffer computeCommandEncoder];
+    commandEncoder.label = commandBuffer.label;
+    
+    [commandEncoder setTexture:TextureGetMetalTexture( params.readTexture ) atIndex:0];
+    [commandEncoder setTexture:TextureGetMetalTexture( params.writeTexture ) atIndex:1];
+
+    //[commandEncoder setBuffer:GetCurrentUBO() offset:0 atIndex:0];
+    [commandEncoder setComputePipelineState:shaders[ shader.index ].computePipeline];
+
+    [commandEncoder dispatchThreadgroups:threadgroups threadsPerThreadgroup:shaders[ shader.index ].threadgroupCounts];
+    [commandEncoder endEncoding];
+    
+    [commandBuffer commit];
+}
