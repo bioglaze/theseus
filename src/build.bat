2055c1fd18da63b1f35fadde75836932156fdd73
Timo Wiren <timo.wiren@gmail.com>
2024-09-29 19:58:45 +0300

    Make build.bat work without VS command prompt

--- src/build.bat
+++ src/build.bat
@@ -1,2 +1,3 @@
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
 cl -c unity.cpp -W4 /DVK_USE_PLATFORM_WIN32_KHR /D_CRT_SECURE_NO_WARNINGS /DSIMD_SSE3 /Iinclude /Ivideo /Icore /I%VULKAN_SDK%\Include /std:c++17 /FC /diagnostics:column
 cl samples\hello\hello.cpp thirdparty\imgui\*.cpp unity.obj -W4 /DVK_USE_PLATFORM_WIN32_KHR /D_CRT_SECURE_NO_WARNINGS /DSIMD_SSE3 /Iinclude /Ivideo /Icore /Ithirdparty/imgui/ /std:c++17 /FC /diagnostics:column /link /LIBPATH:%VULKAN_SDK%\Lib vulkan-1.lib /OUT:..\build\hello.exe
