2279dc44b80ca368675a6de86e83613d0bd94dc2
Timo Wiren <timo.wiren@gmail.com>
2025-06-28 20:32:13 +0300

    [vulkan] Compile shaders as 1.2 instead of 1.3 because apparently Raspi requires it

--- src/compile_deploy_vulkan_shaders.cmd
+++ src/compile_deploy_vulkan_shaders.cmd
@@ -1,21 +1,21 @@
 @echo off
 if not exist ..\build\shaders mkdir ..\build\shaders
-%VULKAN_SDK%/bin/dxc -Ges -spirv -fspv-target-env=vulkan1.3 -E unlitVS -all-resources-bound -T vs_6_5 shaders/hlsl/unlit.hlsl -Fo ../build/shaders/unlit_vs.spv
-%VULKAN_SDK%/bin/dxc -Ges -spirv -fspv-target-env=vulkan1.3 -E unlitPS -all-resources-bound -T ps_6_5 shaders/hlsl/unlit.hlsl -Fo ../build/shaders/unlit_ps.spv
-%VULKAN_SDK%/bin/dxc -Ges -spirv -fspv-target-env=vulkan1.3 -E uiVS -all-resources-bound -T vs_6_5 shaders/hlsl/ui.hlsl -Fo ../build/shaders/ui_vs.spv
-%VULKAN_SDK%/bin/dxc -Ges -spirv -fspv-target-env=vulkan1.3 -E uiPS -all-resources-bound -T ps_6_5 shaders/hlsl/ui.hlsl -Fo ../build/shaders/ui_ps.spv
-%VULKAN_SDK%/bin/dxc -Ges -spirv -fspv-target-env=vulkan1.3 -E fullscreenVS -all-resources-bound -T vs_6_5 shaders/hlsl/fullscreen.hlsl -Fo ../build/shaders/fullscreen_vs.spv
-%VULKAN_SDK%/bin/dxc -Ges -spirv -fspv-target-env=vulkan1.3 -E fullscreenPS -all-resources-bound -T ps_6_5 shaders/hlsl/fullscreen.hlsl -Fo ../build/shaders/fullscreen_ps.spv
-%VULKAN_SDK%/bin/dxc -Ges -spirv -fspv-target-env=vulkan1.3 -E fullscreenAdditivePS -all-resources-bound -T ps_6_5 shaders/hlsl/fullscreen.hlsl -Fo ../build/shaders/fullscreen_additive_ps.spv
-%VULKAN_SDK%/bin/dxc -Ges -spirv -fspv-target-env=vulkan1.3 -E skyboxVS -all-resources-bound -T vs_6_5 shaders/hlsl/skybox.hlsl -Fo ../build/shaders/skybox_vs.spv
-%VULKAN_SDK%/bin/dxc -Ges -spirv -fspv-target-env=vulkan1.3 -E skyboxPS -all-resources-bound -T ps_6_5 shaders/hlsl/skybox.hlsl -Fo ../build/shaders/skybox_ps.spv
-%VULKAN_SDK%/bin/dxc -Ges -spirv -fspv-target-env=vulkan1.3 -E standardVS -all-resources-bound -T vs_6_5 shaders/hlsl/standard.hlsl -Fo ../build/shaders/standard_vs.spv
-%VULKAN_SDK%/bin/dxc -Ges -spirv -fspv-target-env=vulkan1.3 -E standardPS -all-resources-bound -T ps_6_5 shaders/hlsl/standard.hlsl -Fo ../build/shaders/standard_ps.spv
-%VULKAN_SDK%/bin/dxc -Ges -spirv -fspv-target-env=vulkan1.3 -E bloomThreshold -all-resources-bound -T cs_6_5 shaders/hlsl/bloom.hlsl -Fo ../build/shaders/bloom_threshold.spv
-%VULKAN_SDK%/bin/dxc -Ges -spirv -fspv-target-env=vulkan1.3 -E bloomBlur -all-resources-bound -T cs_6_5 shaders/hlsl/bloom.hlsl -Fo ../build/shaders/bloom_blur.spv
-%VULKAN_SDK%/bin/dxc -Ges -spirv -fspv-target-env=vulkan1.3 -E bloomDownsample -all-resources-bound -T cs_6_5 shaders/hlsl/bloom.hlsl -Fo ../build/shaders/downsample.spv
-%VULKAN_SDK%/bin/dxc -Ges -spirv -fspv-target-env=vulkan1.3 -E bloomCombine -all-resources-bound -T cs_6_5 shaders/hlsl/bloom.hlsl -Fo ../build/shaders/bloom_combine.spv
-%VULKAN_SDK%/bin/dxc -Ges -spirv -fspv-target-env=vulkan1.3 -E momentsVS -all-resources-bound -T vs_6_5 shaders/hlsl/moments.hlsl -Fo ../build/shaders/moments_vs.spv
-%VULKAN_SDK%/bin/dxc -Ges -spirv -fspv-target-env=vulkan1.3 -E momentsPS -all-resources-bound -T ps_6_5 shaders/hlsl/moments.hlsl -Fo ../build/shaders/moments_ps.spv
+%VULKAN_SDK%/bin/dxc -Ges -spirv -fspv-target-env=vulkan1.2 -E unlitVS -all-resources-bound -T vs_6_5 shaders/hlsl/unlit.hlsl -Fo ../build/shaders/unlit_vs.spv
+%VULKAN_SDK%/bin/dxc -Ges -spirv -fspv-target-env=vulkan1.2 -E unlitPS -all-resources-bound -T ps_6_5 shaders/hlsl/unlit.hlsl -Fo ../build/shaders/unlit_ps.spv
+%VULKAN_SDK%/bin/dxc -Ges -spirv -fspv-target-env=vulkan1.2 -E uiVS -all-resources-bound -T vs_6_5 shaders/hlsl/ui.hlsl -Fo ../build/shaders/ui_vs.spv
+%VULKAN_SDK%/bin/dxc -Ges -spirv -fspv-target-env=vulkan1.2 -E uiPS -all-resources-bound -T ps_6_5 shaders/hlsl/ui.hlsl -Fo ../build/shaders/ui_ps.spv
+%VULKAN_SDK%/bin/dxc -Ges -spirv -fspv-target-env=vulkan1.2 -E fullscreenVS -all-resources-bound -T vs_6_5 shaders/hlsl/fullscreen.hlsl -Fo ../build/shaders/fullscreen_vs.spv
+%VULKAN_SDK%/bin/dxc -Ges -spirv -fspv-target-env=vulkan1.2 -E fullscreenPS -all-resources-bound -T ps_6_5 shaders/hlsl/fullscreen.hlsl -Fo ../build/shaders/fullscreen_ps.spv
+%VULKAN_SDK%/bin/dxc -Ges -spirv -fspv-target-env=vulkan1.2 -E fullscreenAdditivePS -all-resources-bound -T ps_6_5 shaders/hlsl/fullscreen.hlsl -Fo ../build/shaders/fullscreen_additive_ps.spv
+%VULKAN_SDK%/bin/dxc -Ges -spirv -fspv-target-env=vulkan1.2 -E skyboxVS -all-resources-bound -T vs_6_5 shaders/hlsl/skybox.hlsl -Fo ../build/shaders/skybox_vs.spv
+%VULKAN_SDK%/bin/dxc -Ges -spirv -fspv-target-env=vulkan1.2 -E skyboxPS -all-resources-bound -T ps_6_5 shaders/hlsl/skybox.hlsl -Fo ../build/shaders/skybox_ps.spv
+%VULKAN_SDK%/bin/dxc -Ges -spirv -fspv-target-env=vulkan1.2 -E standardVS -all-resources-bound -T vs_6_5 shaders/hlsl/standard.hlsl -Fo ../build/shaders/standard_vs.spv
+%VULKAN_SDK%/bin/dxc -Ges -spirv -fspv-target-env=vulkan1.2 -E standardPS -all-resources-bound -T ps_6_5 shaders/hlsl/standard.hlsl -Fo ../build/shaders/standard_ps.spv
+%VULKAN_SDK%/bin/dxc -Ges -spirv -fspv-target-env=vulkan1.2 -E bloomThreshold -all-resources-bound -T cs_6_5 shaders/hlsl/bloom.hlsl -Fo ../build/shaders/bloom_threshold.spv
+%VULKAN_SDK%/bin/dxc -Ges -spirv -fspv-target-env=vulkan1.2 -E bloomBlur -all-resources-bound -T cs_6_5 shaders/hlsl/bloom.hlsl -Fo ../build/shaders/bloom_blur.spv
+%VULKAN_SDK%/bin/dxc -Ges -spirv -fspv-target-env=vulkan1.2 -E bloomDownsample -all-resources-bound -T cs_6_5 shaders/hlsl/bloom.hlsl -Fo ../build/shaders/downsample.spv
+%VULKAN_SDK%/bin/dxc -Ges -spirv -fspv-target-env=vulkan1.2 -E bloomCombine -all-resources-bound -T cs_6_5 shaders/hlsl/bloom.hlsl -Fo ../build/shaders/bloom_combine.spv
+%VULKAN_SDK%/bin/dxc -Ges -spirv -fspv-target-env=vulkan1.2 -E momentsVS -all-resources-bound -T vs_6_5 shaders/hlsl/moments.hlsl -Fo ../build/shaders/moments_vs.spv
+%VULKAN_SDK%/bin/dxc -Ges -spirv -fspv-target-env=vulkan1.2 -E momentsPS -all-resources-bound -T ps_6_5 shaders/hlsl/moments.hlsl -Fo ../build/shaders/moments_ps.spv
 pause
 
