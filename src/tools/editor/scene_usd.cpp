d4af346cdfeb2f931b4f74b90f37afeb46cd60a8
Timo Wiren <timo.wiren@gmail.com>
2025-11-28 21:21:41 +0200

    [editor] Use tinyusdz in Makefile and scene_usd.cpp

--- src/tools/editor/scene_usd.cpp
+++ src/tools/editor/scene_usd.cpp
@@ -1,12 +1,46 @@
 #ifdef USD_SUPPORT
-#define NOMINMAX
-#include <pxr/usd/usd/stage.h>
-#include <pxr/usd/usdGeom/xform.h>
+#include "tinyusdz.hh"
+#include "pprinter.hh"
+#include "value-pprint.hh"
 #include <iostream>
+#include <string>
 #include "scene.h"
 
 void LoadUsdScene( const char* path )
 {
-    pxr::UsdStageRefPtr stage = pxr::UsdStage::CreateNew( path );
+    tinyusdz::Stage stage;
+    std::string warn, err;
+    
+    bool ret = tinyusdz::LoadUSDFromFile( path, &stage, &warn, &err );
+
+    if (warn.size())
+    {
+        std::cout << "WARN : " << warn << "\n";
+    }
+
+    if (!ret)
+    {
+        if (!err.empty())
+        {
+            std::cerr << "ERR : " << warn << "\n";
+        }
+        return;
+    }
+
+    // Print Stage(Scene graph)
+    std::cout << tinyusdz::to_string(stage) << "\n";
+  
+    // You can also use ExportToString() as done in pxrUSD 
+    // std::cout << stage.ExportToString() << "\n";
+
+    // stage.metas() To get Scene metadatum, 
+    for (const tinyusdz::Prim &root_prim : stage.root_prims())
+    {
+        std::cout << root_prim.absolute_path() << "\n";
+        // You can traverse Prim(scene graph object) using Prim::children()
+        // See examples/api_tutorial and examples/tydra_api for details.
+    }
+
+    return;
 }
 #endif
