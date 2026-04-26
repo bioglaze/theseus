ec16fd0a94a83a01959f7e367aee1c9b752f942c
Timo Wiren <timo.wiren@gmail.com>
2026-04-26 08:24:58 +0300

    [editor] Grid improvements

--- src/tools/editor/sceneview.cpp
+++ src/tools/editor/sceneview.cpp
@@ -684,20 +684,17 @@ void AddGridLines( float gridStep )
 {
     unsigned i = 0;
 
-    //const float d = 5.0f * gridStep;
-    const float d = 5;
-
-    for (int x = 0; x < 10; ++x)
+    for (int x = 0; x < 11; ++x)
     {
         sceneView.lineBuffer[ i * 2 + 0 ] = Vec3( x * gridStep, 0, 0 );
-        sceneView.lineBuffer[ i * 2 + 1 ] = Vec3( x * gridStep, 0, 20 );
+        sceneView.lineBuffer[ i * 2 + 1 ] = Vec3( x * gridStep, 0, 10 * gridStep );
         ++i;
     }
 
-    for (int z = 0; z < 10; ++z)
+    for (int z = 0; z < 11; ++z)
     {
-        sceneView.lineBuffer[ i * 2 + 0 ] = Vec3( 0, 0, z * gridStep + 5 );
-        sceneView.lineBuffer[ i * 2 + 1 ] = Vec3( 10, 0, z * gridStep + 5 );
+        sceneView.lineBuffer[ i * 2 + 0 ] = Vec3( 0, 0, z * gridStep );
+        sceneView.lineBuffer[ i * 2 + 1 ] = Vec3( 10 * gridStep, 0, z * gridStep );
         ++i;
     }
 }
@@ -705,7 +702,7 @@ void AddGridLines( float gridStep )
 void RenderSceneView( float gridStep )
 {
     AddGridLines( gridStep );
-    teRendererUpdateLineBuffer( sceneView.unlitShader, sceneView.lineBuffer, 40 );
+    teRendererUpdateLineBuffer( sceneView.unlitShader, sceneView.lineBuffer, 60 );
 
     teBeginFrame();
     ImGui::NewFrame();
