ab678363fcabbbfba45d834b68409b4a1544808e
Timo Wiren <timo.wiren@gmail.com>
2026-05-21 20:14:58 +0300

    [vulkan] Use 3-channel normal map, set attenuation to 1 in standard shader

--- src/shaders/hlsl/standard.hlsl
+++ src/shaders/hlsl/standard.hlsl
@@ -143,8 +143,9 @@ float4 standardPS( VSOutput vsOut ) : SV_Target
     float surfaceDistToLight = length( uniforms.lightPosition.xyz - vsOut.positionWS );
     //float depth = (vsOut.projCoord.z + 0.0001f) / vsOut.projCoord.w;
     float3 shadow = max( 0.2f, VSM( surfaceDistToLight, vsOut.projCoord ) );
-    float2 normalTex = texture2ds[ pushConstants.normalMapIndex ].Sample( samplers[ S_LINEAR_REPEAT ], vsOut.uv ).xy;
-    float3 normalTS = float3( normalTex.x, normalTex.y, sqrt( 1 - normalTex.x * normalTex.x - normalTex.y * normalTex.y ) );
+    //float2 normalTex = texture2ds[ pushConstants.normalMapIndex ].Sample( samplers[ S_LINEAR_REPEAT ], vsOut.uv ).xy;
+    float3 normalTS = normalize( texture2ds[ pushConstants.normalMapIndex ].Sample( samplers[ S_LINEAR_REPEAT ], vsOut.uv ).xyz * 2.0 - 1.0 );
+    //float3 normalTS = float3( normalTex.x, normalTex.y, sqrt( 1 - normalTex.x * normalTex.x - normalTex.y * normalTex.y ) );
     float3 normalVS = tangentSpaceTransform( vsOut.tangentVS, vsOut.bitangentVS, vsOut.normalVS, normalTS.xyz );
 
     const float3 N = normalVS;
@@ -210,12 +211,13 @@ float4 standardPS( VSOutput vsOut ) : SV_Target
         
         if (lightDistance < radius)
         {
-            const float attenuation = pointLightAttenuation( length( vecToLightWS ), 1.0f / radius );
+            float attenuation = pointLightAttenuation( length( vecToLightWS ), 1.0f / radius );
+            attenuation = 1.0;
             // FIXME: something wrong with the following line
             //const float3 color = Fd + Fr;
             const float3 color = 1.0f;
             float4 pointLightColor = vk::RawBufferLoad < float4 > (pushConstants.pointLightColorBuf + 16 * lightIndex);
-            accumDiffuseAndSpecular.rgb += (color * pointLightColor.rgb) * attenuation * dotNL * 2.0f;
+            accumDiffuseAndSpecular.rgb += (color * pointLightColor.rgb) * attenuation * dotNL * 1.0f;
             //return float4( pointLightColor.rgb, 1 );
         }
     }
